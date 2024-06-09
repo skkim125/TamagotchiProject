9be9db24c832211ca1434994916766522bd1ce29
skkim125 <kthanks125@gmail.com>
2024-06-09 23:56:03 +0900

    Tamagotchi Project: 코드 1차 간단 리팩토링(print문 및 사용하지 않는 함수 코드 삭제, 함수명 수정, 주석처리된 코드 삭제, 긴 코드문 extension 함수로 작성)

--- TamagotchiProject/Setting/SettingTableViewCell.swift
+++ TamagotchiProject/Setting/SettingTableViewCell.swift
@@ -10,7 +10,7 @@ import UIKit
 class SettingTableViewCell: UITableViewCell {
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: .value1, reuseIdentifier: SettingTableViewCell.id)
+        super.init(style: .value1, reuseIdentifier: reuseIdentifier)
     }
     
     required init?(coder: NSCoder) {
